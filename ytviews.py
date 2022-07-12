b5fefe86805bf3a0e33d56fe4fce01d0bbaecc4f
Arbaj07 <64407123+Arbaj07@users.noreply.github.com>
2022-07-12 11:46:40 +0530

    Update ytviews.py

--- ytviews.py
+++ ytviews.py
@@ -1,20 +1,20 @@
 from selenium import webdriver
 import time
-views = int(input("enter no. of view: "))
-view_time = float(input('enter time for each view: '))
+views = int(input("enter no. of view:1000"))
+view_time = float(input('enter time for each view:5'))
 
 browser1 = webdriver.Chrome()
 browser2 = webdriver.Chrome()
 browser3 = webdriver.Chrome()
 
-# put video link in ''
-for i in range(views):
+# put video link in 'https://www.youtube.com/watch?v=2j7fD92g-gE&t=332s'
+for i in range(1000):
     browser1.get('')
-    time.sleep(view_time)
+    time.sleep(5)
     browser2.get('')
-    time.sleep(view_time)
+    time.sleep(1)
     browser3.get('')
-    time.sleep(view_time)
+    time.sleep(1)
 
 browser1.close()
 browser2.close()
